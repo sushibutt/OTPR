6fa88a26a7a017d8cf6136e4b393a86bd7825277
Joshua <jojosinogba@gmail.com>
2023-04-21 16:28:05 +0800

    About_us added

---
+++ app/src/main/java/com/example/otpr/About_us.kt
@@ -0,0 +1,11 @@
+package com.example.otpr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class About_us : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_about_us)
+    }
+}
