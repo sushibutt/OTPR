c609d9a7bbc95946ecc994dd0a1fec7a7d035733
Joshua <jojosinogba@gmail.com>
2023-04-24 10:30:34 +0800

    Feature implementation

--- app/src/main/java/com/example/otpr/MainActivity.kt
+++ app/src/main/java/com/example/otpr/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.otpr
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,6 +21,41 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+
+        val input = findViewById<EditText>(R.id.editToken)
+        val button = findViewById<Button>(R.id.buttonEnter)
+
+        button.setOnClickListener {
+
+            val token = input.text.toString()
+
+            if (token.isEmpty()) {
+                input.error = "Token is Required"
+                input.requestFocus()
+                return@setOnClickListener
+            }
+
+            RetrofitClient.instance.login(token)
+                .enqueue(object : Callback<LogResponse> {
+                    override fun onResponse(call: Call<LogResponse>, response: Response<LogResponse>) {
+
+                        if (response.body()?.success!!) {
+
+                            val intent = Intent(applicationContext, Dashboard::class.java)
+                            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                            startActivity(intent)
+                        } else {
+                            Toast.makeText(applicationContext, "Error Logging in", Toast.LENGTH_LONG).show()
+                        }
+                    }
+
+                    override fun onFailure(call: Call<LogResponse>, t: Throwable) {
+                        Toast.makeText(applicationContext, t.message, Toast.LENGTH_LONG).show()
+                    }
+
+                })
+        }
+    }
 
 
 
