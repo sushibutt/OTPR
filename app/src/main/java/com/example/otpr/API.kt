ba8069f35f655126b5e676537384eaf0d3374f01
Joshua <jojosinogba@gmail.com>
2023-04-25 22:50:09 +0800

    Added SMS API and SMSResponse Data Class

--- app/src/main/java/com/example/otpr/API.kt
+++ app/src/main/java/com/example/otpr/API.kt
@@ -4,10 +4,16 @@ import retrofit2.Call
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.POST
+
 interface API {
     @FormUrlEncoded
     @POST("authenticate")
     fun login(
-        @Field("link_device_code") link_device_code:String
-    ):Call<LogResponse>
+        @Field("link_device_code") link_device_code: String
+    ): Call<LogResponse>
+
+    @POST("messages")
+    fun sendSMS(
+        @Field("messages") messages: String
+    ): Call<SMSResponse>
 }
---
+++ app/src/main/java/com/example/otpr/SMSResponse.kt
@@ -0,0 +1,3 @@
+package com.example.otpr
+
+data class SMSResponse(val success: Boolean)
